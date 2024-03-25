19fc07c22b5e8cc9bc974966ca5944f2849f3ea5
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-25 16:46:07 +0700

    build: remove unused `lint.cmake` module

--- cmake/lint.cmake
+++
@@ -1,6 +0,0 @@
-add_custom_target(lint
-  COMMAND
-    find "examples" "include" "src" "test"
-    '\(' -name '*.cpp' -o -name '*.hpp' -o -name '*.tpp' '\)'
-    -exec uncrustify -c test/uncrustify/config.cfg -l CPP --replace --no-backup {} '\;'
-  WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
