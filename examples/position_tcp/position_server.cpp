471074bf6001917cd1a65d382a9ee345a5f9c843
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-03-25 16:40:38 +0700

    docs: remove license header in all files (#62)

--- examples/position_tcp/position_server.cpp
+++ examples/position_tcp/position_server.cpp
@@ -1,9 +1,3 @@
-// Copyright (c) 2021-2024 ICHIRO ITS
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 #include <cstdlib>
 #include <iostream>
 #include <list>
